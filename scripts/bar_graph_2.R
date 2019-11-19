ed38244c013e8d65c686d3c8bf26c741d27b4bc3
Sasha Gordin <sashagordin22@gmail.com>
2019-11-18 19:02:40 -0800

    Made changes to the bar_graph_2.R file in order to show prices on the y-axis labels. Additionally changed the scale of the y-axis to make it more readable. Made changes to the index.Rmd file to explain the insights from the chart.

--- scripts/bar_graph_2.R
+++ scripts/bar_graph_2.R
@@ -2,6 +2,7 @@
 library("dplyr")
 library("ggplot2")
 library("tidyr")
+library("scales")
 
 # Pull in dataset
 data_df <-read.csv("data/AB_NYC_2019.csv",stringsAsFactors = FALSE)
@@ -9,14 +10,19 @@ data_df <-read.csv("data/AB_NYC_2019.csv",stringsAsFactors = FALSE)
 # Finds the average price for each neighborhood group
 averaged <- data_df %>%
   group_by(neighbourhood_group) %>%
-  mutate(avg_price = mean(price)) 
+  mutate(avg_price = round(mean(price), 2)) 
+
+# Removes all duplicate rows for which the average price is the same.
+# This leaves data for each neighborhood group and narrows down the dataset.
+averaged <- averaged[!duplicated(averaged$avg_price), ]
+
   
 # Creates bar graph
 avg_price_bar_graph <- 
   ggplot(averaged) +
-  geom_col(
-    mapping = aes(x = neighbourhood_group, y = avg_price)
-  ) +
+  geom_col(mapping = aes(x = neighbourhood_group, y = avg_price)) +
+  theme(plot.title = element_text(hjust=0.5)) + #center the title
+  scale_y_continuous(labels = dollar) + #makes the y-axis labels in dollars
   labs(
     title = "Average Price of NYC Airbnb Listings by Neighborhood Areas", 
     x = "Neighborhood Group", # x-axis label
