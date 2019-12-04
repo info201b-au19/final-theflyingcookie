f338af62a0324d0d7b578eff0836d4ef76db9d97
theflyingcookie <valeriet2000@gmail.com>
2019-12-04 02:26:35 -0800

    Added first page details

--- FinalDeliverable/app_ui.R
+++ FinalDeliverable/app_ui.R
@@ -27,11 +27,27 @@ title <- tabPanel(
         sidebarPanel(
             # A static content element: a 2nd level header that displays text
             h2("Questions Our Project Investigated"),
+            p()
             
         ),
         mainPanel(
-            h3("Header size 3 here "),
-            p("blah blah blah blah blah"),            
+            h3("Introduction to the Project"),
+            p("The purpose of this project is to evaluate how Airbnb listing information compares within NYC.
+            Airbnb is gaining marketshare in the tourist accommodation market, which has been dominated by 
+            hotels, hostels, and motels in the past, and pricing has changed overtime, since it has become
+            much more accessible to a larger audience of people. Services such as Uber, Doordash, Fiverr, where
+            individuals offer services is only something that has come about relatively recently, and as one of
+            the most prominent 'brokers', our group believes it is important to find the impact Airbnb will have
+            on the future."), 
+            
+            h3("Data Source"),
+            a("NYC Airbnb Dataset from Kaggle", href = "https://www.kaggle.com/dgomonov/new-york-city-airbnb-open-data"),
+            
+            p(),
+            p(),
+            
+            img(src = "https://video-images.vice.com/articles/5c3cfa75a4786f0007380ad0/lede/1547500483819-shutterstock_665040679-copy.jpeg?crop=1xw%3A0.843644544431946xh%3Bcenter%2Ccenter&resize=2000%3A*",
+                height="90%", width="90%"),
         )
     )
 )
@@ -66,7 +82,7 @@ page_two <- tabPanel(
     
     sidebarLayout(
         sidebarPanel(
-            # Wideget 1: first state for comparison
+            # Widget 1: first state for comparison
             selectInput(
                 inputId = "mapFeature",
                 label = "Select a feature you would like to explore",
@@ -100,7 +116,7 @@ page_three <- tabPanel(
     
     sidebarLayout(
         sidebarPanel(
-            # Wideget 1: first state for comparison
+            # Widget 1: first state for comparison
             selectInput(
                 inputId = "barFeature",
                 label = "Select a feature you would like to explore",
